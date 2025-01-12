76904630a141fa97e77e8c0016ba15ae9b6b9e7b
Akio Kanashiki <akio@mail.com>
2025-01-12 15:05:43 +0900

    modified

--- brontes-aws/lib/BrontesStack.ts
+++ brontes-aws/lib/BrontesStack.ts
@@ -1,7 +1,8 @@
-import { Duration, Stack, StackProps } from "aws-cdk-lib";
+import { CfnOutput, Duration, RemovalPolicy, Stack, StackProps } from "aws-cdk-lib";
 import { IpProtocol, SubnetType, Vpc } from "aws-cdk-lib/aws-ec2";
-import { Cluster, ContainerImage, FargateService, FargateTaskDefinition } from "aws-cdk-lib/aws-ecs";
+import { Cluster, ContainerImage, FargateService, FargateTaskDefinition, LogDriver } from "aws-cdk-lib/aws-ecs";
 import { ApplicationLoadBalancer, ApplicationProtocol, ApplicationTargetGroup, TargetType } from "aws-cdk-lib/aws-elasticloadbalancingv2";
+import { LogGroup, RetentionDays } from "aws-cdk-lib/aws-logs";
 import { Construct } from "constructs";
 
 export class BrontesStack extends Stack {
@@ -10,13 +11,20 @@ export class BrontesStack extends Stack {
         super(scope, id, props);
 
         const maxAzs = 2;
-        const listnerPort = 80;
+        const listenerPort = 80;
         const containerPort = 8080;
 
         const taskDefinition = new FargateTaskDefinition(this, 'TaskDefinition', { cpu: 512, memoryLimitMiB: 4096 });
         taskDefinition.addContainer('application', {
             image: ContainerImage.fromAsset(`${__dirname}/../../brontes-api`),
             portMappings: [{ containerPort }],
+            logging: LogDriver.awsLogs({
+                streamPrefix: 'application',
+                logGroup: new LogGroup(this, 'ApplicationLogGroup', {
+                    retention: RetentionDays.ONE_DAY,
+                    removalPolicy: RemovalPolicy.DESTROY,
+                })
+            })
         });
 
         const vpc = new Vpc(this, 'Vpc', {
@@ -28,13 +36,16 @@ export class BrontesStack extends Stack {
         const lb = new ApplicationLoadBalancer(vpc, 'LoadBalancer', {
             vpc, vpcSubnets: { subnetType: SubnetType.PUBLIC }, internetFacing: true,
         });
-        const lister = lb.addListener('HttpListener', { protocol: ApplicationProtocol.HTTP, port: listnerPort });
+        const lister = lb.addListener('HttpListener', { protocol: ApplicationProtocol.HTTP, port: listenerPort });
 
         const cluster = new Cluster(vpc, 'ApplicationCluster', {
             vpc, enableFargateCapacityProviders: true,
         });
         const service = new FargateService(cluster, 'ApplicationService', {
-            taskDefinition, cluster, vpcSubnets: { subnetType: SubnetType.PRIVATE_WITH_EGRESS },
+            taskDefinition, cluster, vpcSubnets: { subnetType: SubnetType.PRIVATE_WITH_EGRESS }, assignPublicIp: false,
+            capacityProviderStrategies: [{ capacityProvider: 'FARGATE_SPOT', weight: 1 }],
+            minHealthyPercent: 0, maxHealthyPercent: 200, circuitBreaker: { enable: true, rollback: true },
+            desiredCount: 1,
         });
 
         const targetGroup = new ApplicationTargetGroup(this, 'TargetGroup', {
@@ -42,11 +53,14 @@ export class BrontesStack extends Stack {
             targets: [service],
             healthCheck: {
                 path: '/actuator/health',
-                interval: Duration.seconds(5),
+                interval: Duration.seconds(5), timeout: Duration.seconds(2),
                 healthyThresholdCount: 2, unhealthyThresholdCount: 2,
+                healthyHttpCodes: '200',
             },
             deregistrationDelay: Duration.seconds(0),
         });
         lister.addTargetGroups('TargetGroups', { targetGroups: [targetGroup] });
+
+        new CfnOutput(this, 'ApplicationEndpoint', { value: `http://${lb.loadBalancerDnsName}:${listenerPort}` });
     }
 }
