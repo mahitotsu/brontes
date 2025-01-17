f18a3f8d5db38d7e0ce733d36e03adc80bc2b09a
Akio Kanashiki <akio@mail.com>
2025-01-18 02:30:39 +0900

    modified

--- brontes-api/src/it/java/com/mahitotsu/brontes/api/HealthCheckIT.java
+++ brontes-api/src/it/java/com/mahitotsu/brontes/api/HealthCheckIT.java
@@ -1,14 +1,10 @@
 package com.mahitotsu.brontes.api;
 
-import java.util.Properties;
-
 import org.junit.jupiter.api.Test;
 
 public class HealthCheckIT {
 
     @Test
     public void test() {
-        Properties properties = System.getProperties();
-        properties.list(System.out); 
     }
 }
