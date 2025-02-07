c9085e6ba5a372e70792454201dd6a90d1aefcd9
Akio Kanashiki <akio@mail.com>
2025-02-08 00:59:47 +0900

    modified

--- brontes-api/src/main/java/com/mahitotsu/brontes/api/Main.java
+++ brontes-api/src/main/java/com/mahitotsu/brontes/api/Main.java
@@ -3,10 +3,8 @@ package com.mahitotsu.brontes.api;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
-@EnableCaching
 public class Main {
     
     public static void main(final String ...args) {
