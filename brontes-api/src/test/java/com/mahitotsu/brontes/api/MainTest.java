c5d3b5d33a982f28685dc7d0ace42f1bfb407caf
Akio Kanashiki <akio@mail.com>
2025-02-21 00:41:07 +0900

    modified

---
+++ brontes-api/src/test/java/com/mahitotsu/brontes/api/MainTest.java
@@ -0,0 +1,12 @@
+package com.mahitotsu.brontes.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class MainTest {
+    
+    @Test
+    public void testMain() {
+    }
+}
