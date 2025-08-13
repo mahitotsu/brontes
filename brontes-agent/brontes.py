a7936d2bd94d8a00ef68687dbed432ddb0667b97
akio.kanashiki <akio@mail.com>
2025-08-13 19:04:51 +0900

    modified

--- brontes-agent/brontes.py
+++ brontes-agent/brontes.py
@@ -1,10 +1,21 @@
 from bedrock_agentcore.runtime import BedrockAgentCoreApp
+from strands import Agent
+from strands.models import BedrockModel
+
+model = BedrockModel(
+    model_id="us.amazon.nova-micro-v1:0",
+    region_name="us-east-1",
+)
+agent = Agent(model=model,)
 
 app = BedrockAgentCoreApp()
 
 @app.entrypoint
 def invoke(payload):
-    return ""
+    """Process user input and return a response"""
+    user_message = payload.get("prompt", "Hello")
+    result = agent(user_message)
+    return {"result": result}
 
 if __name__ == "__main__":
     app.run()
